1b0074db272ed108498cded39402a0cd098e13a7
berukokoko <beru2491@gmail.com>
2018-07-16 19:59:03 +0900

    変更

--- app/src/main/java/jp/techacademy/rin/andou/javalog/Human.java
+++ app/src/main/java/jp/techacademy/rin/andou/javalog/Human.java
@@ -23,6 +23,6 @@ public class Human extends Animal  {
     //animalクラスからのsayメソッド。
     @Override
     public void say() {
-        Log.d("javatest", "私の名前は"+ this.name +"です。" + "年は"+this.age+"歳です");
+
     }
 }
