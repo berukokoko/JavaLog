06e1d7103d28eaa48aa04ffcc7fa60f069cc8926
berukokoko <beru2491@gmail.com>
2018-07-16 20:45:32 +0900

    変更

--- app/src/main/java/jp/techacademy/rin/andou/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/rin/andou/javalog/MainActivity.java
@@ -8,19 +8,13 @@ public class MainActivity extends AppCompatActivity {
     String name;
     int age;
 
-    public MainActivity(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     //Humanをインスタンス化する。
-    Human human = new Human("name", age);
+    Human human = new Human("安藤", 25, "料理");
     //sayメソッドを呼ぶ
         human.say();
         Log.d("javatest","私の名前は"+human.name +"です。");
