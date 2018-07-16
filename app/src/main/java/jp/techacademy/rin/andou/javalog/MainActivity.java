aa9c121b93b4b846dab39c4d03d21e92a9777185
berukokoko <beru2491@gmail.com>
2018-07-16 18:24:03 +0900

    変更

--- app/src/main/java/jp/techacademy/rin/andou/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/rin/andou/javalog/MainActivity.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
+    String name;
+    int age;
+
+    public MainActivity(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String name;
-        int age;
 
-            public MainActivity(String name, int age){
-            this.name = name;
-            this.age = age;
-        }
 
-        //Humanをインスタンス化する。
-       Human human = new Human("name",age);
-        //sayメソッドを呼ぶ。
+
+    //Humanをインスタンス化する。
+    Human human = new Human("name", age);
+    //sayメソッドを呼ぶ。
         Human.say();
-        Log.d("javatest", "私の名前は" + Human.name + "です。");
-        Log.d("javatest", "年は" + Human.age + "です。");
-        //thineメソッドを呼ぶ。
+        Log.d("javatest","私の名前は"+Human.name +"です。");
+        Log.d("javatest","年は"+Human.age +"です。");
+    //thineメソッドを呼ぶ。
         Human.think();
     }
 
